1ee349f150570ce37b7ca1a8f3d62e1e40c762c3
andcastillo <andcastillo@gmail.com>
2019-07-16 14:00:29 -0500

    import script working

--- src/build/kaggleBuildDB.js
+++ src/build/kaggleBuildDB.js
@@ -2,9 +2,6 @@
 
 const fs = require('fs');
 
-const dataFolder = '/home/acastillo/Documents/kaggle/champs-scalar-coupling/';
-const type = '3JHN';
-
 function build(path, type) {
 
   var train = fs.readFileSync(`${path}/train-${type}.csv`).toString().split('\n');
@@ -48,4 +45,7 @@ function build(path, type) {
 module.exports = build;
 
 
+//const dataFolder = '/home/acastillo/Documents/kaggle/champs-scalar-coupling/';
+//const type = '3JHN';
 
+build('data/', '1JHC');
