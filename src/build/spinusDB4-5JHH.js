0829468c848dcdd2a742cdd9f2edb8328d54cd3f
andres castillo <andcastillo@gmail.com>
2019-09-23 15:56:39 -0500

    add spinus data importer

---
+++ src/build/spinusDB4-5JHH.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const OCLE = require('openchemlib-extended');
+const Util = OCLE.Util;
+const fs = require('fs');
+const stat = require('ml-stat/array');
+
+const path = require('path');
+const outputFolder = 'data/';
+
+
+// const stat = require('ml-stat/array');
+
+
+let folder = '/Users/acastillo//Documents/dataNMR/spinus/'
+let data = fs.readdirSync(folder).filter(file => file.indexOf('.mol') >= 0);
+
+let nMols = data.length;
+let nSamples = 10000;
+let maxSphereSize = 5;
+let nHoses = 3;
+
+let types = ['4JHH', '5JHH', '6JHH', '7JHH'];
+let dbs = {};
+for (let type of types) {
+    dbs[type] = [{}, {}, {}];
+}
+
+for (let n = 0; n < nSamples; n++) {
+    console.log(n)
+    let randomSample = Math.round(Math.random() * nMols);
+    let molfile = fs.readFileSync(path.join(folder, data[randomSample])).toString();
+    let spinus = JSON.parse(fs.readFileSync(path.join(folder, data[randomSample].replace('.mol', '.json'))));
+    let molmap = OCLE.Molecule.fromMolfileWithAtomMap(molfile);
+    let molecule = molmap.molecule;
+    let diaIDs = molecule.getGroupedDiastereotopicAtomIDs({ atomLabel: "H" });
+    diaIDs.sort(function (a, b) {
+        return b.counter - a.counter;
+    });
+
+    let atomMap = {};
+    diaIDs.forEach(diaID => {
+        diaID.atoms.forEach(atom => atomMap[atom] = diaID);
+    });
+
+    // HOSE codes from Diasterotopic atom IDs.
+    for (let k = 0; k < diaIDs.length; k++) {
+        diaIDs[k].hose = Util.getHoseCodesFromDiastereotopicID(diaIDs[k].oclID, {
+            maxSphereSize: maxSphereSize,
+            type: 0
+        });
+    }
+
+    // console.log(JSON.stringify(spinus.map(s => s.j)));
+
+    spinus.forEach(atom => {
+        if (atom.j) {
+            atom.j.forEach(j => {
+                if (j.distance > 3) {
+                    let type = `${j.distance}JHH`;
+                    let db = dbs[type];
+                    let hose = atomMap[j.assignment[0]].hose;
+                    try {
+                        for (let i = maxSphereSize - 1; i >= maxSphereSize - nHoses; i--) {
+                            if (!db[i - nHoses + 1][hose[i]]) {
+                                db[i - nHoses + 1][hose[i]] = [];
+                            }
+                            db[i - nHoses + 1][hose[i]].push(j.coupling);
+                        }
+                    } catch(e) {
+                        console.log(type);
+                        console.log(hose);
+                    }
+                    
+                }
+            });
+        }
+    });
+}
+
+function getStats(entry) {
+    const minMax = stat.minMax(entry);
+    return {
+        min: Math.round(minMax.min * 1000) / 1000,
+        max: Math.round(minMax.max * 1000) / 1000,
+        ncs: entry.length,
+        mean: Math.round(stat.mean(entry) * 1000) / 1000,
+        median: Math.round(stat.median(entry) * 1000) / 1000,
+        std: Math.round(stat.standardDeviation(entry, false) * 1000) / 1000
+    };
+}
+
+for (let type of types) {
+    let db = dbs[type];
+    fs.writeFileSync(`${outputFolder}/spinus${type}-full.json`, JSON.stringify(db));
+    for(let i = 0; i < db.length; i++) {
+        let dbi = db[i];
+        let keys = Object.keys(dbi);
+        for(let key of keys) {
+            dbi[key] = getStats(dbi[key]);
+            dbi[key].cop2 = [dbi[key].mean, 0, 0];
+        }
+    }
+    fs.writeFileSync(`${outputFolder}/spinus${type}.json`, JSON.stringify(db));
+
+}
--- src/build/unionDB.js
+++ src/build/unionDB.js
@@ -4,13 +4,18 @@ const fs = require('fs');
 
 const path = 'data/';
 // const types = ['1JHC', '2JHC', '3JHC', '1JHN', '2JHN', '3JHN', '2JHH', '3JHH'];
-const types = ['2JHH', '3JHH'];
+const types = ['2JHH', '3JHH', '4JHH', '5JHH'];
 let db = {};
 types.forEach(type => {
-    db[type] = JSON.parse(fs.readFileSync(`${path}/kaggle-abs${type}.json`).toString());
+    if (fs.existsSync(`${path}/kaggle-abs${type}.json`))
+        db[type] = JSON.parse(fs.readFileSync(`${path}/kaggle-abs${type}.json`).toString());
+    else if (fs.existsSync(`${path}/spinus${type}.json`)) {
+        db[type] = JSON.parse(fs.readFileSync(`${path}/spinus${type}.json`).toString());
+        db[type] = [{}, {}, db[type][2]];
+    }
     db[type].forEach(x => console.log(Object.keys(x).length));
     //console.log(db[type].length)
 });
 
-fs.writeFileSync(`${path}/cheminfo-abs${types[0].substring(2)}.json`, JSON.stringify(db));
+fs.writeFileSync(`${path}/cheminfo-abs-spinus${types[0].substring(2)}.json`, JSON.stringify(db));
 
